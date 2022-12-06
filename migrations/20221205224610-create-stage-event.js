937b295372742c55848030888b102cc532e1a16b
Kyle DAmato <kyledamato4@gmail.com>
2022-12-05 21:09:50 -0500

    finished assignment

--- migrations/20221205224610-create-stage-event.js
+++ migrations/20221205224610-create-stage-event.js
@@ -1,22 +1,32 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     /**
      * Add altering commands here.
      *
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
+    await queryInterface.createTable("stage_event", {
+      stage_events_id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      stage_id: { type: Sequelize.INTEGER, allowNull: false },
+      event_id: { type: Sequelize.INTEGER, allowNull: false },
+    });
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add reverting commands here.
      *
      * Example:
      * await queryInterface.dropTable('users');
      */
-  }
+    await queryInterface.dropTable("stage_event");
+  },
 };
--- migrations/20221205224614-create-stage.js
+++ migrations/20221205224614-create-stage.js
@@ -1,22 +1,30 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     /**
      * Add altering commands here.
      *
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
+    await queryInterface.createTable("stage", {
+      stage_id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      stage_name: { type: Sequelize.STRING, allowNull: false },
+    });
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add reverting commands here.
      *
      * Example:
      * await queryInterface.dropTable('users');
      */
-  }
+  },
 };
